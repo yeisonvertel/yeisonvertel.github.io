e2ea3972679d562c2c8a0310220598517fc409dc
Yeison Vertel <yeisonvertel1@gmail.com>
2023-09-11 13:43:49 -0500

    DashboardPJ

---
+++ orders.js
@@ -0,0 +1,14 @@
+const Orders = [
+    {
+        productName: 'Facebook',
+        productNumber: '5.700',
+        paymentStatus: '124',
+        status: '125'
+    },
+    {
+        productName: 'Instagram',
+        productNumber: '2.128',
+        paymentStatus: '89',
+        status: '26'
+    },
+]
